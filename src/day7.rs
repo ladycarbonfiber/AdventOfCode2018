652ee6cc3767c4185caff59f80acb02862824c95
tom <ssyrmik@gmail.com>
2018-12-10 09:43:18 -0500

    day7pt1

--- src/day7.rs
+++ src/day7.rs
@@ -6,6 +6,7 @@ use petgraph::algo::*;
 use std::collections::HashSet;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+
 pub fn solve(part: Part) -> String {
     let input = read_input(7);
     let node_regex = Regex::new(r"Step (\w) .+ step (\w)").unwrap();
@@ -14,80 +15,54 @@ pub fn solve(part: Part) -> String {
             let mut out = String::new();
             let mut node_table = HashMap::new();
             let mut deps = Graph::<&str, &str>::new();
-            let edges:Vec<(&str, &str)> = input.lines().map(|x| parse_line(x)).collect();
+            let edges: Vec<(&str, &str)> = input.lines().map(|x| parse_line(x)).collect();
             let mut nodes = HashSet::new();
-            edges.iter().for_each(|x| {nodes.insert(x.0); nodes.insert(x.1);});
-            for node in nodes{
+            edges.iter().for_each(|x| {
+                nodes.insert(x.0);
+                nodes.insert(x.1);
+            });
+            for node in nodes {
                 node_table.insert(node, deps.add_node(node));
             }
-            let adjusted_edges:Vec<(NodeIndex<u32>, NodeIndex<u32>)> =edges.iter()
+            let adjusted_edges: Vec<(NodeIndex<u32>, NodeIndex<u32>)> = edges.iter()
                 .map(|x| (*node_table.get(x.0).unwrap(), *node_table.get(x.1).unwrap()))
                 .collect();
             deps.extend_with_edges(adjusted_edges);
-            let sorted:NodeIndex = *node_table.get("V").unwrap();//toposort(&deps, None).unwrap()[0];
-            println!("root {}", deps[sorted]);
             let mut indx_deq = VecDeque::new();
             let mut processed = HashSet::new();
-            indx_deq.push_front(sorted);
-            indx_deq.push_front(*node_table.get("U").unwrap());
-            indx_deq.push_front(*node_table.get("E").unwrap());
-            indx_deq.push_front(*node_table.get("B").unwrap());
-            //out.push_str(deps[*indx_deq[0]]);
-            //processed.insert(deps[*indx_deq[0]]);
-
-            println!("{:?}", deps);
-            let mut first = 0;
-            loop{
+            for c in b'A'.. = b'Z' {
+                if deps.neighbors_directed(*node_table.get(&(c as char).to_string().as_str()).unwrap(), Direction::Incoming).
+                    fold(true, |x, y| x && processed.contains(deps[y])) {
+                    indx_deq.push_back(*node_table.get(&(c as char).to_string().as_str()).unwrap());
+                }
+            }
+            loop {
                 let maybe_current = indx_deq.pop_front();
-                match maybe_current{
-                    Some(current)=>{
+                match maybe_current {
+                    Some(current) => {
                         if deps.neighbors_directed(current, Direction::Incoming)
-                            .fold(true, |x,y| x && processed.contains(deps[y]))
-                           {
+                            .fold(true, |x, y| x && processed.contains(deps[y]))
+                            {
                                 out.push_str(deps[current]);
                                 processed.insert(deps[current]);
-                                let mut next_nodes:Vec<NodeIndex> = deps.neighbors_directed(current, Direction::Outgoing).collect();
-                               for node in indx_deq.drain(..){
-                                   next_nodes.push(node);
-                               }
-                                next_nodes.sort_by(|x,y|deps[*x].cmp(deps[*y]).reverse()
+                                let mut next_nodes: Vec<NodeIndex> = deps.neighbors_directed(current, Direction::Outgoing).collect();
+                                for node in indx_deq.drain(..) {
+                                    next_nodes.push(node);
+                                }
+                                next_nodes.sort_by(|x, y| deps[*x].cmp(deps[*y]).reverse()
                                     .then(deps[*x].len().cmp(&deps[*y].len())));
                                 for node in next_nodes.drain(..) {
                                     if !indx_deq.contains(&node) {
                                         indx_deq.push_front(node);
                                     }
                                 }
-                               println!("{}", out);
-                               println!("{:?}", indx_deq.iter().map(|x| deps[*x]).collect::<Vec<_>>());
-                            //next_nodes.iter().for_each(|x| indx_deq.push_back(x));
-                        }else{
-                            if first < 20{
-                                println!("Not satisfied {}", deps[current]);
-                                first += 1;
-                            }
-                            //
+                            } else {
                             indx_deq.push_back(current);
                         }
-                    },
-                    None=> break
+                    }
+                    None => break
                 }
             }
-
-
-                    //.for_each(|x| indx_deq.push_back(x))
-
-
-//            for line in input.lines(){
-//                match node_regex.captures(&line){
-//                    Some(t) => {
-//                        println!("{}, {}", t.get(1).map_or("", |m| m.as_str()),
-//                                 t.get(2).map_or("", |m| m.as_str()));
-//
-//                    },
-//                    None => ()
-//                }
-//            }
-            println!("{}", out.len());
             out
         }
         Part::Two => {
@@ -95,14 +70,16 @@ pub fn solve(part: Part) -> String {
         }
     }
 }
-fn parse_line(line:&str)->(&str, &str){
+
+fn parse_line(line: &str) -> (&str, &str) {
     let node_regex = Regex::new(r"Step (\w) .+ step (\w)").unwrap();
     match node_regex.captures(line) {
-        Some(t)=>{
+        Some(t) => {
             (t.get(1).map_or("", |m| m.as_str()), t.get(2).map_or("", |m| m.as_str()))
-        }None=>{
+        }
+        None => {
             println!("Error parsing {}", line);
-            ("","")
+            ("", "")
         }
     }
 }
